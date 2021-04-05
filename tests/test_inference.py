aa56e136d8d4b821c6e9b5f79e1ad1947a030ab9
Robin Cole <robmarkcole@gmail.com>
2021-04-05 07:07:10 +0100

    run black

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -3,19 +3,21 @@ import torch
 from PIL import Image
 
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+model = torch.hub.load("ultralytics/yolov5", "yolov5s")
 
 # Images
-for f in ['zidane.jpg']:  # download 2 images, 'bus.jpg'
-    print(f'Downloading {f}...')
-    torch.hub.download_url_to_file('https://github.com/ultralytics/yolov5/releases/download/v1.0/' + f, f)
+for f in ["zidane.jpg"]:  # download 2 images, 'bus.jpg'
+    print(f"Downloading {f}...")
+    torch.hub.download_url_to_file(
+        "https://github.com/ultralytics/yolov5/releases/download/v1.0/" + f, f
+    )
 
-img1 = Image.open('zidane.jpg')  # PIL image
+img1 = Image.open("zidane.jpg")  # PIL image
 
 results = model(img1, size=640)  # includes NMS
 
 # Results
-results.print()  
+results.print()
 # print(results.tolist())
 
 # Data
