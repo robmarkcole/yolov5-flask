bcfe4c2719f4173e8f5d29d9c9eab251457eefcb
Robin Cole <robmarkcole@gmail.com>
2021-04-06 05:00:33 +0100

    Update test_inference.py

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -5,12 +5,12 @@ from PIL import Image
 # Model
 model = torch.hub.load("ultralytics/yolov5", "yolov5s", pretrained=True, force_reload=True)
 
-# img = Image.open("zidane.jpg")  # PIL image, succeeds
+img = Image.open("zidane.jpg")  # PIL image, succeeds
 
-# Reading bytes fails
-with open("zidane.jpg", "rb") as file:
-    img_bytes = file.read()
-img = Image.open(io.BytesIO(img_bytes))
+# # Reading bytes fails - issue https://github.com/ultralytics/yolov5/issues/2702
+# with open("zidane.jpg", "rb") as file:
+#     img_bytes = file.read()
+# img = Image.open(io.BytesIO(img_bytes))
 
 print(img.size) # validate image
 results = model(img, size=640)  # includes NMS
