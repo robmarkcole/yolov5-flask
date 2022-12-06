e3d437ae2da21c5427c9afd0ae820890da79b906
Robin Cole <robmarkcole@gmail.com>
2022-12-06 12:44:44 +0000

    solve issue

--- restapi.py
+++ restapi.py
@@ -10,7 +10,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-DETECTION_URL = "/v1/object-detection/yolov5s"
+DETECTION_URL = "/v1/object-detection/yolov5"
 
 
 @app.route(DETECTION_URL, methods=["POST"])
@@ -22,16 +22,14 @@ def predict():
         image_file = request.files["image"]
         image_bytes = image_file.read()
         img = Image.open(io.BytesIO(image_bytes))
-        results = model(img, size=640)
-        data = results.pandas().xyxy[0].to_json(orient="records")
-        return data
-
+        results = model(img, size=640) # reduce size=320 for faster inference
+        return results.pandas().xyxy[0].to_json(orient="records")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask api exposing yolov5 model")
     parser.add_argument("--port", default=5000, type=int, help="port number")
+    parser.add_argument('--model', default='yolov5s', help='model to run, i.e. --model yolov5s')
     args = parser.parse_args()
 
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)  # force_reload = recache latest code
-    model.eval()
+    model = torch.hub.load('ultralytics/yolov5', args.model)
     app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
